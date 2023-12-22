e4d80433db6a2f14ffd75411b8e859d33ba37fc5
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2023-12-22 10:39:25 +0100

    Update LSP_Bad.java

--- LSP_Bad.java
+++ LSP_Bad.java
@@ -1,71 +1,57 @@
-class Apartment {
+public class Apartment {
     protected String apartmentName;
     protected int squareFootage;
     protected int numberOfBedrooms;
 
-    public Apartment(String apartmentName, int numberOfBedrooms) {
-        this.apartmentName = apartmentName;
-        this.numberOfBedrooms = numberOfBedrooms;
-    }
-
     public void setSquareFootage(int sqft) {
         squareFootage = sqft;
-    }
-
-    public String getApartmentName() {
-        return apartmentName;
-    }
-
-    public int getNumberOfBedrooms() {
-        return numberOfBedrooms;
-    }
-
-    public void upgrade() {
-        squareFootage += 40;
-        numberOfBedrooms += getAdditionalBedrooms();
-    }
-
-    protected int getAdditionalBedrooms() {
-        return 0; // Default behavior for the base class
     }
 }
 
 class PenthouseSuite extends Apartment {
     public PenthouseSuite() {
-        super("Penthouse", 4);
+        numberOfBedrooms = 4;
+        setName("Penthouse");
+    }
+
+    public void setName() {
+        apartmentName = "Penthouse";
     }
 
     @Override
-    protected int getAdditionalBedrooms() {
-        return 1; // Penthouse has an additional bedroom
+    public void setSquareFootage(int sqft) {
+        squareFootage = sqft;
     }
 }
 
 class Studio extends Apartment {
     public Studio() {
-        super("Studio", 0);
+        numberOfBedrooms = 0;
+        setName("Studio");
+    }
+
+    public void setName() {
+        apartmentName = "Studio";
+    }
+
+    @Override
+    public void setSquareFootage(int sqft) {
+        squareFootage = sqft;
     }
 }
 
 class UnitUpgrader {
     public void upgrade(Apartment apartment) {
-        apartment.upgrade();
+        apartment.squareFootage += 40;
+
+        if (!apartment.apartmentName.equals("Studio")) {
+            apartment.numberOfBedrooms += 1;
+        }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Apartment penthouse = new PenthouseSuite();
-        penthouse.setSquareFootage(2000);
-
-        Apartment studio = new Studio();
-        studio.setSquareFootage(500);
-
-        UnitUpgrader upgrader = new UnitUpgrader();
-        upgrader.upgrade(penthouse);
-        upgrader.upgrade(studio);
-
-        System.out.println("Penthouse: " + penthouse.getApartmentName() + ", Bedrooms: " + penthouse.getNumberOfBedrooms() + ", Square Footage: " + penthouse.squareFootage);
-        System.out.println("Studio: " + studio.getApartmentName() + ", Bedrooms: " + studio.getNumberOfBedrooms() + ", Square Footage: " + studio.squareFootage);
+        // Your main code here
     }
 }
